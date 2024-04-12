92fa37ad10866c8304a89617ebce5dfbeca5ff18
thinhquoc <rayker09@gmail.com>
2024-04-12 16:25:26 +0700

    update

--- main.js
+++ main.js
@@ -36,38 +36,12 @@ const slider = [
     },
 ];
 
-const sliderContainer = document.getElementById('slider');
+const slide = document.getElementById('slider');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 
-let currentStartIndex = 0;
-
-function showSlide(index) {
-    sliderContainer.innerHTML = '';
-
-    for (let i = currentStartIndex; i < currentStartIndex + 2; i++){
-        const slide = slider[i];
-        sliderContainer.innerHTML += `
-            <img src="${slide.image}" alt="${slide.alt}" />
-        `
-    }
+function slideShow() {
+    
 }
 
-function nextSlide() {
-    if (currentStartIndex + 2 < slider.length){
-        currentStartIndex += 1;
-        showSlide();
-    }
-}
 
-function prevSlide() {
-    if (currentStartIndex > 0){
-        currentStartIndex -= 1;
-        showSlide();
-    }
-}
-
-showSlide();
-
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
